34d5748b652656f484c9c90744320b2b36e0f393
martnx <martinmart2002@gmail.com>
2024-07-16 19:04:31 +0800

    final commit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+//Account data list
 const accountList = [
+    "user:admin",
     "martin:martin23", 
     "johndoe:420420", 
     "joshmendoza:mendoza2024", 
@@ -11,20 +13,46 @@ const btn = document.querySelector("#btn");
 const userDisplay = document.querySelector("#user-para");
 const passDisplay = document.querySelector("#pass-para");
 
+// Test Code
+// for(x of accountList){
+//     x = x.split(":");
+//     console.log(`Username: ${x[0]} \nPassword: ${x[1]}`);
+// }
+
+//This code block activate when the button is clicked
 btn.addEventListener("click", () => {
     const searchAccount = input.value.toLowerCase();
-    console.log(searchAccount);
-    input.value = " ";
+    let accountFound = false;
+    // console.log(searchAccount);
+    input.value = "";
     input.focus();
-    for(const account of accountList){
-        splitList = account.split(":");
+    para.textContent = "";
+    userDisplay.textContent = "";
+    passDisplay.textContent = "";
+
+    // for(x of accountList){
+    // x = x.split(":");
+    // console.log(`Username: ${x[0]} \nPassword: ${x[1]}`);
+    // }
+
+    // Checking the condition if account on the list 
+    for(splitList of accountList){
+        splitList = splitList.split(":");
+        console.log(`Username: ${splitList[0]} \nPassword: ${splitList[1]}`);
+
+
         if(searchAccount === splitList[0]){
             console.log(`Username: ${splitList[0]} \nPassword: ${splitList[1]}`);
-            break;
-        }else{
-            console.log("no account found");
+            para.textContent += "Account found!";
+            userDisplay.textContent += "Username:  " + splitList[0];
+            passDisplay.textContent += "Password: "+ splitList[1];
+            accountFound = true;
+            break;   
+        }
+        if(searchAccount == ""){
+            para.textContent = "Input valid value only!"
+            userDisplay.textContent = "";
+            passDisplay.textContent = "";
         }
     }
-
-
 });
