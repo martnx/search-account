59fc7a57937cd4b9db6d76c59d4bbba06b54e2e1
martnx <martinmart2002@gmail.com>
2024-07-16 15:49:56 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const accountList = [
+    "martin:martin23", 
+    "johndoe:420420", 
+    "joshmendoza:mendoza2024", 
+    "mark:0942069", 
+    "angelo:angelo2002"];
+
+const para = document.querySelector("#para-display");
+const input = document.querySelector("input-search");
+const btn = document.querySelector("#btn");
+const userDisplay = document.querySelector("#user-para");
+const passDisplay = document.querySelector("#pass-para");
+
+btn.addEventListener("click"){
+    console.log("test")
+}
